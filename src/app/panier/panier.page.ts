a25bf36f9d52d8e48ec6864159aa18f7206b36a9
Mozart921 <119967032+Mozart921@users.noreply.github.com>
2024-06-03 13:55:58 +0200

    Add files via upload

---
+++ src/app/panier/panier.page.ts
@@ -0,0 +1,185 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-panier',
+  templateUrl: './panier.page.html',
+  styleUrls: ['./panier.page.scss'],
+})
+export class PanierPage implements OnInit {
+  panier: any[] = [];
+  connected: boolean = false;
+  userID: string = '';
+  total: number;
+  totalHT: number;
+  email: string | null;
+  commande: any[] = [];
+  envoi: boolean = false;
+  commandeInfo: any = {
+    prenom: '',
+    nom: '',
+    adresse1: '',
+    adresse2: '',
+    ville: '',
+    region: '',
+    codePostal: '',
+    pays: '',
+    telMobile: '',
+    nomCarte: '',
+    numCarte: '',
+    dateExp: '',
+    cvv: ''
+  };
+
+  constructor(
+    public afDB: AngularFireDatabase,
+    public afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState.subscribe(auth => {
+      if (!auth) {
+        console.log('non connecté');
+        this.connected = false;
+      } else {
+        console.log('connecté: ' + auth.uid);
+        this.email = auth.email;
+        this.userID = auth.uid;
+        this.connected = true;
+      }
+      
+    })
+  }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.connected = true;
+        this.userID = user.uid;
+        this.getPanier();
+      } else {
+        this.connected = false;
+        this.userID = '';
+      }
+    });
+  }
+
+  confirmCommande() {
+    this.envoi = !this.envoi;
+  }
+
+  getPanier() {
+    if (!this.connected) {
+      console.log('Veuillez vous connecter pour voir votre panier.');
+      return;
+    }
+
+    this.afDB.list('panier/', ref => ref.orderByChild('userId').equalTo(this.userID)).snapshotChanges().subscribe(actions => {
+      this.panier = [];
+      actions.forEach(action => {
+        const data = action.payload.val();
+        this.panier.push({
+          id: action.key,
+          name: action.payload.exportVal().produitName,
+          categorie: action.payload.exportVal().produitCategorie,
+          produitId: action.payload.exportVal().produitId,
+          prix: action.payload.exportVal().productPrix,
+          choixDate: action.payload.exportVal().choixDate,
+          imageUrl: action.payload.exportVal().imageUrl,
+          quantity: action.payload.exportVal().quantité
+        });
+      });
+      this.updateTotal();
+    });
+  }
+
+  updateTotal() {
+    this.total = this.panier.reduce((acc, product) => acc + (product.prix * product.quantity), 0);
+    this.totalHT = this.panier.reduce((acc, product) => acc + ((product.prix * product.quantity) * 0.7), 0)
+  }
+
+  deleteProductFromPanier(productId: string) {
+    this.afDB.list('panier/').remove(productId).then(() => {
+      this.panier = this.panier.filter(product => product.id !== productId);
+      this.updateTotal();
+      console.log('Produit supprimé avec succès!');
+    }).catch(error => {
+      console.error('Erreur lors de la suppression du produit:', error);
+    });
+  }
+
+  passerCommande() {
+    if (!this.userID) {
+      console.log('Veuillez vous connecter pour passer une commande.');
+      return;
+    }
+  
+    // Récupérer les données du panier de l'utilisateur
+    this.afDB.list('panier/', ref => ref.orderByChild('userId').equalTo(this.userID)).snapshotChanges().pipe(take(1)).subscribe(actions => {
+      this.commande = []; // Initialiser la variable commande à un tableau vide
+      actions.forEach(action => {
+        const data = action.payload.val();
+        this.commande.push({ // Ajouter les données de chaque produit du panier dans la commande
+          id: action.key,
+          name: action.payload.exportVal().produitName,
+          categorie: action.payload.exportVal().produitCategorie,
+          produitId: action.payload.exportVal().produitId,
+          prix: action.payload.exportVal().productPrix,
+          choixDate: action.payload.exportVal().choixDate,
+          imageUrl: action.payload.exportVal().imageProduit,
+          quantity: action.payload.exportVal().quantité
+        });
+      });
+  
+      // Une fois que tous les produits du panier ont été ajoutés à la commande, vous pouvez maintenant passer la commande
+      const commande = {
+        userId: this.userID,
+        email: this.email,
+        produits: this.commande, // Utiliser la commande créée à partir du contenu du panier
+        total: this.total, // Utiliser le total du panier
+        totalHT: this.totalHT, // Utiliser le total HT du panier
+        dateCommande: new Date().toISOString(),
+        nom: this.commandeInfo.nom,
+        prenom: this.commandeInfo.prenom,
+        adresse1: this.commandeInfo.adresse1,
+        adresse2: this.commandeInfo.adresse2,
+        ville: this.commandeInfo.ville,
+        region: this.commandeInfo.region,
+        CP: this.commandeInfo.codePostal,
+        pays: this.commandeInfo.pays,
+        telMobile: this.commandeInfo.telMobile,
+        nomCarte: this.commandeInfo.nomCarte,
+        numCarte: this.commandeInfo.numCarte,
+        dateExp: this.commandeInfo.dateExp,
+        CVV: this.commandeInfo.cvv,
+        recu: 'en cours'
+      };
+  
+      // Ajouter la commande à la base de données
+      this.afDB.list('commandes/').push(commande).then(() => {
+        console.log('Commande passée avec succès!');
+        // Vous pouvez également vider le panier après la commande
+        actions.forEach(action => {
+          const key = action.key;
+          if (key) {
+            this.afDB.list('panier/').remove(key).then(() => {
+              console.log('Produit supprimé du panier avec succès!');
+            }).catch(error => {
+              console.error('Erreur lors de la suppression du produit du panier:', error);
+            });
+          }
+        });
+  
+        // Réinitialiser le panier et le total après avoir supprimé tous les produits
+        this.panier = [];
+        this.updateTotal();
+      }).catch(error => {
+        console.error('Erreur lors du passage de la commande:', error);
+      });
+    });
+  }
+  
+  
+}
